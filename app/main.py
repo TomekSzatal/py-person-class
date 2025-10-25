71da8924f55b281ffde98c87d2b1bd795dcb3f31
Tomasz Szatałowicz <szatalowicztomek@gmail.com>
2025-10-25 14:01:54 +0200

    quest

--- app/main.py
+++ app/main.py
@@ -1,8 +1,19 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    for person_data in people:
+        Person(person_data["name"], person_data["age"])
+    for person_data in people:
+        person = Person.people[person_data["name"]]
+        if "wife" in person_data and person_data["wife"] is not None:
+            person.wife = Person.people.get(person_data["wife"])
+        if "husband" in person_data and person_data["husband"] is not None:
+            person.husband = Person.people.get(person_data["husband"])
+    return list(Person.people.values())
